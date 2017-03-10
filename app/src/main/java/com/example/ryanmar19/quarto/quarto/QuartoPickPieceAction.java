181711a313a25daef037a8a9ab2f17ef353b9886
maggieryan3 <ryanmar19@up.edu>
2017-03-10 10:11:12 -0800

    3/10 basic homework (algorithm needs to be completed)

--- app/src/main/java/com/example/ryanmar19/quarto/quarto/QuartoPickPieceAction.java
+++ app/src/main/java/com/example/ryanmar19/quarto/quarto/QuartoPickPieceAction.java
@@ -1,9 +1,22 @@
 package com.example.ryanmar19.quarto.quarto;
 
+import com.example.ryanmar19.quarto.game.GamePlayer;
+import com.example.ryanmar19.quarto.game.actionMsg.GameAction;
+
 /**
  * Created by maggi on 3/10/2017.
  */
 
-public class QuartoPickPieceAction {
+public class QuartoPickPieceAction extends GameAction {
+
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public QuartoPickPieceAction(GamePlayer player) {
+        super(player);
+    }
+
     Piece pickedPiece;
 }
--- app/src/main/java/com/example/ryanmar19/quarto/quarto/QuartoPlayPieceAction.java
+++ app/src/main/java/com/example/ryanmar19/quarto/quarto/QuartoPlayPieceAction.java
@@ -1,10 +1,23 @@
 package com.example.ryanmar19.quarto.quarto;
 
+import com.example.ryanmar19.quarto.game.GamePlayer;
+import com.example.ryanmar19.quarto.game.actionMsg.GameAction;
+
 /**
  * Created by maggi on 3/10/2017.
  */
 
-public class QuartoPlayPieceAction {
+public class QuartoPlayPieceAction extends GameAction {
+
+    /**
+     * constructor for GameAction
+     *
+     * @param player the player who created the action
+     */
+    public QuartoPlayPieceAction(GamePlayer player) {
+        super(player);
+    }
+
     Piece playedPiece;
     int row;
     int col;
